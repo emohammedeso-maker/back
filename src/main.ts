1b2c69e9c86c0aa541f538c63f6b3b439abf68f5
Esraa Elnajjar <esowork9000@gmail.com>
2026-03-24 19:41:16 +0200

    sdfvdfvs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
    console.log("MYSQL_URL:", process.env.MYSQL_URL);
